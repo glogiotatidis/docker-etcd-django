ca153596ca7297d846551c33a76e2a3a804caa36
Giorgos Logiotatidis <seadog@sealabs.net>
2014-09-22 18:45:13 +0300

    Dynamic db.

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,12 @@ FROM debian
 RUN apt-get update
 RUN apt-get install -y python-pip
 
+ADD https://github.com/kelseyhightower/confd/releases/download/v0.5.0/confd-0.5.0-linux-amd64 /usr/local/bin/confd
+RUN chmod +x /usr/local/bin/confd
+
 ADD app/ /app
+ADD etc/ /etc
+ADD usr/ /usr
+
 
 RUN pip install -r /app/requirements.txt
--- app/app/settings.py
+++ app/app/settings.py
@@ -56,12 +56,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+DATABASES = {'default': dj_database_url.config()}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
